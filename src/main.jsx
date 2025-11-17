ad8a0dca609172eb8c90bb5d4d739aa9684bcc9e
kairolopes <151247602+kairolopes@users.noreply.github.com>
2025-11-17 20:41:21 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './design.css';
-// 🟢 ESTA LINHA É ESSENCIAL PARA CARREGAR OS ESTILOS
-import './index.css' 
+import App from './App'
+
+// AQUI TEM QUE TER ESTA LINHA!!!
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
