b8223d95e316b6ed9c3cd676b860e5df70684f58
kairolopes <151247602+kairolopes@users.noreply.github.com>
2025-11-17 15:14:06 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// CORREÇÃO: Adiciona a extensão do arquivo para que o Netlify o encontre
-import app from './app.jsx' 
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <app />
+    <App />
   </React.StrictMode>,
 )
