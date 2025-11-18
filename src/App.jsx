65d864b7ba2f4d58ce802e7ca25ca5389712c096
kairolopes <151247602+kairolopes@users.noreply.github.com>
2025-11-17 21:18:22 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./index.css";
 import React, { useState, useEffect } from "react";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
@@ -152,5 +153,6 @@ const LoginScreen = () => {
 };
 
 export default App;
+
 
 
