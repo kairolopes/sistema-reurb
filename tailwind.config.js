3dcdad74264e44de477a1d072cccd059a1dd3ba2
kairolopes <151247602+kairolopes@users.noreply.github.com>
2025-11-17 22:16:45 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+module.exports = {
+  // ESSA PARTE É A MAIS IMPORTANTE! 
+  // Ela diz ao Tailwind onde procurar as classes que você usou.
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Inclui todos os arquivos .jsx na pasta src
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
